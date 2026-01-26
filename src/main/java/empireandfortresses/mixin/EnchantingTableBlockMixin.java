706f91b5b081e5f197886d14c0ba01f6f54b4033
Bill3621 <billnick427@gmail.com>
2026-01-26 20:26:53 +0100

    disable enchanting tables

---
+++ src/main/java/empireandfortresses/mixin/EnchantingTableBlockMixin.java
@@ -0,0 +1,33 @@
+package empireandfortresses.mixin;
+
+import net.minecraft.block.EnchantingTableBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.llamalad7.mixinextras.sugar.Local;
+
+@Mixin(EnchantingTableBlock.class)
+public class EnchantingTableBlockMixin {
+
+    @Inject(at = @At("HEAD"), method = "onUse", cancellable = true)
+    // @Local means you don't have to get all parameters in original order but you
+    // can just pick the ones you need
+    private void init(CallbackInfoReturnable<ActionResult> info, @Local World world, @Local PlayerEntity player) {
+        info.cancel();
+        info.setReturnValue(ActionResult.PASS);
+
+        // This runs twice apparently? (like twice on the server??)
+        // TODO: fix that
+        if (!world.isClient) {
+            player.sendMessage(
+                    Text.literal("Enchanting Tables are disabled in Empires and Fortresses."), false);
+        }
+    }
+}
