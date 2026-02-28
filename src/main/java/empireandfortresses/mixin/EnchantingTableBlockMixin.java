cb31c00610853235e9060c2127d4a9963725d894
RandomFridge <firgde@yahoo.co.jp>
2026-02-28 21:02:59 +0100

    Redirect Enchanting screen to a custom one

--- src/main/java/empireandfortresses/mixin/EnchantingTableBlockMixin.java
+++ src/main/java/empireandfortresses/mixin/EnchantingTableBlockMixin.java
@@ -1,10 +1,15 @@
 package empireandfortresses.mixin;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.EnchantingTableBlock;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,20 +19,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.llamalad7.mixinextras.sugar.Local;
 
+import empireandfortresses.screen.CustomEnchantmentScreenHandler;
+
 @Mixin(EnchantingTableBlock.class)
 public class EnchantingTableBlockMixin {
 
     @Inject(at = @At("HEAD"), method = "onUse", cancellable = true)
     // @Local means you don't have to get all parameters in original order but you
     // can just pick the ones you need
-    private void init(CallbackInfoReturnable<ActionResult> info, @Local World world, @Local PlayerEntity player,
-            @Local Hand hand) {
+    private void init(CallbackInfoReturnable<ActionResult> info, @Local BlockPos pos, @Local BlockState state, @Local World world,
+            @Local PlayerEntity player, @Local Hand hand) {
         info.cancel();
-        info.setReturnValue(ActionResult.PASS);
 
-        if (!world.isClient && hand == Hand.MAIN_HAND) {
-            player.sendMessage(
-                    Text.literal("Enchanting Tables are disabled in Empires and Fortresses."), false);
+        if (world.isClient) {
+            info.setReturnValue(ActionResult.SUCCESS);
+        } else {
+            player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+            info.setReturnValue(ActionResult.CONSUME);
         }
+    }
+
+    @Inject(at = @At("RETURN"), method = "createScreenHandlerFactory", cancellable = true)
+    private void redirectScreenHandler(BlockState state, World world, BlockPos pos, CallbackInfoReturnable<NamedScreenHandlerFactory> info, @Local Text text) {
+        info.setReturnValue(new SimpleNamedScreenHandlerFactory((syncId, inventory, player) -> new CustomEnchantmentScreenHandler(syncId, inventory, ScreenHandlerContext.create(world, pos)), text));
     }
 }
